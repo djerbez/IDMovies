4591760924e63285c6601e8d83370045e549be75
djerbez <102421146+djerbez@users.noreply.github.com>
2023-02-16 19:03:51 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import Movie from "./movie";
+import "./App.css";
+import generatePosterPath from "./helpers/generatePosterPath";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    color: "black",
+    width: "800px",
+    height: "400px",
+    textAlign: "center",
+  },
+};
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [clickedMovie, setClickedMovie] = useState();
+  function openModal(movie) {
+    setModal(true);
+    setClickedMovie(movie);
+  }
+  function closeModal() {
+    setModal(false);
+  }
+  async function getMovies() {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=f28aa71682075a83b20e0e121c7582e3&language=en-US&page=1"
+    );
+    const data = await response.json();
+    setMovies(data.results);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }
+  useEffect(() => {
+    getMovies();
+  }, []);
+  if (loading) {
+    return <div className="loader"></div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>IDMovies</h1>
       </header>
+      <div className="movies">
+        {movies.map((movie) => (
+          <Movie openModal={openModal} movie={movie} />
+        ))}
+      </div>
+      <Modal
+        isOpen={modal}
+        style={customStyles}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <div
+          style={{
+            backgroundImage: `url(${generatePosterPath(
+              clickedMovie?.poster_path
+            )})`,
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+            backgroundSize: "100% 100%",
+            opacity: "0.5",
+          }}
+        >
+          <h1>{clickedMovie?.original_title}</h1>
+        </div>
+      </Modal>
     </div>
   );
 }
