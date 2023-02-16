1d9011324640b47aa72b188bbbd21d0e090dfcb8
djerbez <ivordjerbez@gmail.com>
2023-02-16 19:31:08 +0100

    update

--- src/movie/index.js
+++ src/movie/index.js
@@ -2,8 +2,8 @@ import generatePosterPath from "../helpers/generatePosterPath";
 const Movie = ({movie, openModal}) => {
     return (
         <div onClick={()=>{openModal(movie)}} className="movie">
-          <div className="text">{movie.original_title}</div>
           <img src= {generatePosterPath(movie?.poster_path)}></img>
+          <div className="text">{movie.original_title}</div>
           </div>
           )
 }
